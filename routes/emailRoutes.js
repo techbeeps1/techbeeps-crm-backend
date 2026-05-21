157945994763c990fe7594b05bd3db29609f2bc3
guri <guri77566@gmail.com>
2026-05-21 16:23:31 +0530

    New Valuation form improve and fix error and bugs

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -6,5 +6,5 @@ const router = express.Router();
 router.post('/send-otp', sendOtp);
 router.post('/verify-otp', verifyOtp);
 router.post('/send_email', sendEmail);
-
+// Add more email-related routes as needed
 module.exports = router;
